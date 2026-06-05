883a4405be1dee683ba1ce1034d10a369c7b69fd
YURI KOGA SANTOS <144365959+yurikoga@users.noreply.github.com>
2026-06-05 14:03:28 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function openMenu(){
+    document.getElementById("sidebar").classList.add("active");
+}
+
+function closeMenu(){
+    document.getElementById("sidebar").classList.remove("active");
+}
